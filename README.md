0633b7a8b117bc661ca028689a61797af2b1e5e8
caleb <caleb.a.mccarthy@gmail.com>
2020-06-14 08:18:30 -0500

    Uploade compiling boost example

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cmake Boost example code
+
+### Sample code taken from
+
+https://www.boost.org/doc/libs/1_66_0/doc/html/boost_asio/tutorial/tuttimer1/src.html
+
+
+### Dependencies
+
+1. Boost 1.67.
+   Installed on a raspberry pi with `sudo apt-get install libboost1.67-all-dev`
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+      std::cout << "Begin Wait!" << std::endl;
+
+  boost::asio::io_context io;
+
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
+
+  std::cout << "Wait Finished" << std::endl;
+
+  return 0;
 }
